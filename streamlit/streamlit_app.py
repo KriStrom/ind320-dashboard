941d0cb09d4d2406aaa514cad59191794e8c57a8
Kristian Strøm <kristian.strom@nmbu.no>
2025-09-19 12:43:12 +0200

    page setup

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -1,3 +1,24 @@
 import streamlit as st
 
-st.title("Kristian Strøm's streamlit app for IND320")
+# --- Page setup ---
+page_home = st.Page(
+    page="pages/home.py",
+    title="Home",
+    default=True,
+)
+
+page_data_table = st.Page(
+    page="pages/data_table.py",
+    title="Data table",
+)
+
+page_data_advanced = st.Page(
+    page="pages/data_advanced.py",
+    title="Data table - advanced",
+)
+
+
+# Navigation menu
+pg = st.navigation(pages=[page_home, page_data_table, page_data_advanced])
+# Run navigation
+pg.run()
