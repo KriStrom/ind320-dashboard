40f0a0788220590a6170b7ea24eaa1b275338569
Kristian Strøm <kristian.strom@nmbu.no>
2025-09-19 15:24:07 +0200

    showing the data in the data table page

--- streamlit/pages/data_table.py
+++ streamlit/pages/data_table.py
@@ -1,7 +1,15 @@
 import streamlit as st
 from utils.data_loader import get_csv_data
+import pandas as pd
 
-st.title("Data table (simple)")
-
+# Page title
+st.title("Meteo data for the 1st month of 2025")
+ 
+# Load data
 data_meteo = get_csv_data("data/open-meteo-subset.csv")
-st.dataframe(data_meteo.head())
+
+
+# Transform the data
+data_meteo["time"] = pd.to_datetime(data_meteo["time"])
+df_meteo_jan = data_meteo[data_meteo["time"].dt.month == 1]
+df_meteo_jan
--- streamlit/pages/home.py
+++ streamlit/pages/home.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
+# --- Initializations ---
+# Data variables
 if "data_meteo" not in st.session_state:
     st.session_state.data_meteo = None
 
+# --- Frontend ---
 st.title("Kristian Strøm's streamlit app for IND320")
+st.write("Use the navigation menu to find the content.")
